c47bfe66e6405d9a1f59de3c6a8bad4104763998
Danny Tuppeny <danny@tuppeny.com>
2019-12-09 18:20:07 +0000

    Remove "Open Widget Sample" command
    This is being replaced with code-lens (see https://github.com/Dart-Code/Dart-Code/issues/2151).

--- src/commands/sdk.ts
+++ src/commands/sdk.ts
@@ -4,8 +4,8 @@ export class SdkCommands {
 	// TODO: Find an easy way to share the API signature class.
 	constructor(context: vs.ExtensionContext, private dartExtensionApi: any) {
 
-		context.subscriptions.push(vs.commands.registerCommand("flutter.createSampleProject",
-			(_) => this.runFunctionIfSupported(dartExtensionApi.flutterCreateSampleProject)));
+		// context.subscriptions.push(vs.commands.registerCommand("flutter.createSampleProject",
+		// 	(_) => this.runFunctionIfSupported(dartExtensionApi.flutterCreateSampleProject)));
 	}
 
 	private async runFunctionIfSupported<T>(f: () => Promise<T>): Promise<T> {
@@ -18,6 +18,6 @@ export class SdkCommands {
 	}
 
 	private showApiMismatchError(): void {
-		vs.window.showErrorMessage("The installed version of the Dart extension does not support this feature. Please update the Dart extension.");
+		vs.window.showErrorMessage("The installed version of the Dart extension does not support this feature.");
 	}
 }
